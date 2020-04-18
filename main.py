383f9f7b6abbcc59d4cf821ceddfeb334991541c
APascualIbiza <36167893+APascualIbiza@users.noreply.github.com>
2020-04-18 17:49:44 +0200

    Add files via upload
    Secret key is eliminated from code.

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from flask import Flask
 # from flask_mysqldb import MySQL
+from config import Config
+import os
 
 app = Flask(__name__)
 app.debug = True
-app.secret_key = 'secret123'
+#app.secret_key = 'secret123'
+app.secret_key = os.environ.get('SECRET_KEY')
 
 # Config MySQL
-app.config['MYSQL_HOST'] = '192.168.0.20'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'abastos'
+app.config['MYSQL_HOST'] = os.environ.get('SECRET_KEY')
+app.config['MYSQL_USER'] = os.environ.get('SECRET_KEY')
+app.config['MYSQL_PASSWORD'] = os.environ.get('MYSQL_PASSWORD')
 app.config['MYSQL_DB'] = 'appBD'
 app.config['MYSQL_PORT'] = 3306
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
