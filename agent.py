7ce72b54c1fae138ae48d040cfed0d090ec3bfca
APascualIbiza <36167893+APascualIbiza@users.noreply.github.com>
2020-04-26 22:42:19 +0200

    Add files via upload
    agent.py with use of docker secrets

--- agent.py
+++ agent.py
@@ -3,11 +3,16 @@
 import pymysql
 import datetime
 
+with open ("/run/secrets/mysql_userr", "r") as secrets:
+    user = secrets.readline().replace("\n","")
+
+with open ("/run/secrets/mysql_rpass", "r") as secrets:
+    password = secrets.readline().replace("\n","")
 
 def convert2str(record):
     res = []
     for item in record:
-        if item is None:
+        if item == None:
             res.append('NULL')
         elif type(item) == str:
             res.append('"' + item.replace('"', '\\"') + '"')
@@ -17,53 +22,51 @@ def convert2str(record):
             res.append(str(item))
     return ','.join(res)
 
-
 def copy_table(src_name, src_cursor, dst_name, dst_cursor):
-    sql = 'select * from %s' % src_name
+    sql = 'select * from %s'%src_name
     src_cursor.execute(sql)
     res = src_cursor.fetchall()
     cnt = 0
     for record in res:
         val_str = convert2str(record)
         try:
-            sql = 'insert into %s values(%s)' % (dst_name, val_str)
+            sql = 'insert into %s values(%s)'%(dst_name, val_str)
             dst_cursor.execute(sql)
             cnt += 1
         except Exception as e:
             print(sql, e)
     return cnt
 
-
 def update_time(dst_cursor):
 
-    now = datetime.datetime.now()
-    sql = 'update healthcheck set reg_date="%s" WHERE service="webapp"' % (now)
+    now=datetime.datetime.now()
+    sql = 'update healthcheck set reg_date="%s" WHERE service="webapp"'%(now)
     try:
         dst_cursor.execute(sql)
 
     except Exception as e:
-        print(sql, e)
+            print(sql, e)
 
 
 def main():
 
-    mysql = pymysql.connect(host="db", user="root", passwd="abastos", db="appBD", port=3306)
-    mysql1 = pymysql.connect(host="db", user="root", passwd="abastos", db="information_schema", port=3306)
-    mysql2 = pymysql.connect(host="db", user="root", passwd="abastos", db="nextcloud", port=3306)
+    mysql = pymysql.connect(host="db", user=user, passwd=password, db="appBD", port=3306)
+    mysql1 = pymysql.connect(host="db", user=user, passwd=password, db="information_schema", port=3306)
+    mysql2 = pymysql.connect(host="db", user=user, passwd=password, db="nextcloud", port=3306)
 
-    # create cursor
+    #create cursor
     dst_cur = mysql.cursor()
     src_cur1 = mysql1.cursor()
     src_cur2 = mysql2.cursor()
+:q
+    #delete records and regenerate tables
+    delete1 = dst_cur.execute("TRUNCATE TABLE app_activity")
+    delete2 = dst_cur.execute("TRUNCATE TABLE app_users")
+    delete3 = dst_cur.execute("TRUNCATE TABLE app_authtoken")
+    delete4 = dst_cur.execute("TRUNCATE TABLE app_status")
 
-    # delete records and regenerate tables
-    dst_cur.execute("TRUNCATE TABLE app_activity")
-    dst_cur.execute("TRUNCATE TABLE app_users")
-    dst_cur.execute("TRUNCATE TABLE app_authtoken")
-    dst_cur.execute("TRUNCATE TABLE app_status")
-
-    copy_table("oc_activity", src_cur2, "app_activity", dst_cur)
-    copy_table("oc_users", src_cur2, "app_users", dst_cur)
+    copy_table("oc_activity", src_cur2,"app_activity", dst_cur)
+    copy_table("oc_users", src_cur2 , "app_users", dst_cur)
     copy_table("oc_authtoken", src_cur2, "app_authtoken", dst_cur)
     copy_table("GLOBAL_STATUS", src_cur1, "app_status", dst_cur)
     update_time(dst_cur)
@@ -74,8 +77,7 @@ def main():
     src_cur1.close()
     src_cur2.close()
 
-    print("Update_done")
+    print "update_done"
 
-
-if __name__ == '__main__':
+if __name__ =='__main__':
     main()
